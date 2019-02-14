7396f18afcb537be9204d52916912db34082a373
Marchello00 <nagov-mark@mail.ru>
2019-02-14 23:47:18 +0300

    Archivating added(and 'done' message)

--- backup.sh
+++ backup.sh
@@ -1,5 +1,8 @@
 mkdir archive
 for name in $(find ./test -name "*.cpp")
 do
-	cp $name  archive/${name##*/};
+	cp $name  archive/${name##*/}
 done
+tar -cf archive.tar archive/*
+rm -rf archive
+echo done
