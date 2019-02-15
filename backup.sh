82ba460e7418a56c7049eca21940ef163e60d810
Marchello00 <nagov-mark@mail.ru>
2019-02-15 12:05:30 +0300

    Now with params and help message, but still name collisions

--- backup.sh
+++ backup.sh
@@ -1,8 +1,51 @@
-mkdir archive
-for name in $(find ./test -name "*.cpp")
+function help() {
+	echo "Usage: ./backup.sh [-c <catalog name> -a <archive name>] <extention list>"
+	exit 1
+}
+
+CATALOG_NAME="archive"
+ARCHIVE_NAME=$CATALOG_NAME
+
+while [[ $# -gt 0 ]]
 do
-	cp $name  archive/${name##*/}
+	key=$1
+	case $key in
+	-c)
+		CATALOG_NAME="$2"
+		shift
+		shift
+		;;
+	-a)
+		ARCHIVE_NAME="$2"
+		shift
+		shift
+		;;
+	-*)
+		echo "Wrong param"
+		help
+		;;
+	*)
+		break
+		;;
+	esac
 done
-tar -cf archive.tar archive/*
-rm -rf archive
+
+if [ $# -eq 0 ]
+then 
+	echo "Empty extention list"
+	help
+fi
+
+mkdir $CATALOG_NAME
+
+while [[ $# -gt 0 ]]
+do
+	for name in $(find ./test -name "*.$1")
+	do
+		cp $name $CATALOG_NAME/${name##*/}
+	done
+	shift
+done
+
+tar -cf ${ARCHIVE_NAME}.tar $CATALOG_NAME/*
 echo done
